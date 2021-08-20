7b4e6ed26080a5fd3c4bf3f57fd26b1d3d1b3de4
Kamille Parks <kamilleiparks@gmail.com>
2021-08-20 15:34:28 -0700

    Add bare minimum Calendar

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,26 @@
 import {initializeBlock} from '@airtable/blocks/ui';
 import React from 'react';
 
-function HelloWorldApp() {
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+function SampleApp() {
     // YOUR CODE GOES HERE
-    return <div>Hello world 🚀</div>;
+    return (
+    <FullCalendar
+        plugins={[dayGridPlugin]}
+        headerToolbar={{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth'
+        }}
+        initialView='dayGridMonth'
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+  />
+  )
 }
 
-initializeBlock(() => <HelloWorldApp />);
+initializeBlock(() => <SampleApp />);
